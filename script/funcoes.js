3b825a0f1b5e66adeff9ff92761736657ced6bc6
Davi <149123949+Davi-Souza-Dev@users.noreply.github.com>
2024-05-19 17:01:25 -0300

    Mobile first finalizado

---
+++ script/funcoes.js
@@ -0,0 +1,28 @@
+// Menu Mobile
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+//Carrosel 
+var swiper = new Swiper(".mySwiper", {});
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
